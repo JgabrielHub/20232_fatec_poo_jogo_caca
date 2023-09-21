59420bf9947aa93ebfd25c3ccb0ee268a4ed442a
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-09-21 15:50:12 -0300

    ap07(poo-p2): intro personagem

---
+++ Personagem.java
@@ -0,0 +1,18 @@
+public class Personagem {
+
+    //atributos ou propriedades
+    String nome;
+    int energia;
+    int fome;
+    int sono;
+    //comportamento (métodos)
+    void cacar(){
+        System.out.println(nome + " Caçando...");
+    }
+    void comer(){
+        System.out.println(nome + " Comendo...");
+    }
+    void dormir(){
+        System.out.println(nome + " Dormindo...");
+    }
+}
---
+++ TestePersonagem.java
@@ -0,0 +1,9 @@
+public class TestePersonagem{
+    public static void main(String[] args) {
+        var p = new Personagem();  
+        p.nome = "José";
+        p.cacar();
+        p.comer();
+        p.dormir();
+    }
+}
